1a0e3076f7e959b9efbd3566c8853a5b7f9e87f7
Mary <137189389+Maryfrontend24@users.noreply.github.com>
2024-11-12 16:43:54 +0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-
-createRoot(rootElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App';
+//
+// const rootElement = document.getElementById('root');
+//
+// createRoot(rootElement).render(
+//   <StrictMode>
+//
+//
+//
+//
+//     <App />
+//   </StrictMode>,
+// );
