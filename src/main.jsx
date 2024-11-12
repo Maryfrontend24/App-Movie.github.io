760a896ac1b2c9490d9bd9304924097525e99ce6
Mary <137189389+Maryfrontend24@users.noreply.github.com>
2024-11-12 14:41:35 +0300

    new project

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './components/App/App.jsx'
+
+
+const rootElement = document.getElementById('root')
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
