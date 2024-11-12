55ce8e6ecd08c9f6e2df827f770f2b04d61072af
Mary <137189389+Maryfrontend24@users.noreply.github.com>
2024-11-12 16:22:05 +0300

    edd files

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,19 @@
-
-import js from '@eslint/js'
-import globals from 'globals'
-import eslintReact from 'eslint-plugin-react'
+import js from '@eslint/js';
+import globals from 'globals';
+import eslintReact from 'eslint-plugin-react';
 import eslintReactHooks from 'eslint-plugin-react-hooks';
 import eslintReactRefresh from 'eslint-plugin-react-refresh';
 import prettierPlugin from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginHtml from 'eslint-plugin-html'
-import eslintConfigReactApp from 'eslint-config-react-app'
-import eslintAllyPlugin from 'eslint-plugin-jsx-a11y'
-import eslintImportPlugin from 'eslint-plugin-import'
+import eslintPluginHtml from 'eslint-plugin-html';
+import eslintConfigReactApp from 'eslint-config-react-app';
+import eslintAllyPlugin from 'eslint-plugin-jsx-a11y';
+import eslintImportPlugin from 'eslint-plugin-import';
 
 export default [
-  { ignores: ['dist','node_modules', 'coverage', 'eslint.config.js', '**/*.config.js'] },
+  {
+    ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js', '**/*.config.js'],
+  },
   {
     files: ['**/*.{js,jsx}'],
 
@@ -28,32 +29,25 @@ export default [
     },
     settings: { react: { version: '18.3.1' } },
     plugins: {
-      "react":eslintReact,
-      "react-hooks": eslintReactHooks,
-      "react-refresh": eslintReactRefresh,
-      "prettier": prettierPlugin,
-      "eslint-plugin-html": eslintPluginHtml,
-      "jsx-a11y":eslintAllyPlugin,
-      "eslint-config-react-app":eslintConfigReactApp,
-      "import":eslintImportPlugin,
+      react: eslintReact,
+      'react-hooks': eslintReactHooks,
+      'react-refresh': eslintReactRefresh,
+      prettier: prettierPlugin,
+      'eslint-plugin-html': eslintPluginHtml,
+      'jsx-a11y': eslintAllyPlugin,
+      'eslint-config-react-app': eslintConfigReactApp,
+      import: eslintImportPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...prettierPlugin.configs.recommended.rules,
       ...eslintConfigPrettier.rules,
 
-      semi: "error",
-      "prefer-const": "error",
+      semi: 'error',
+      'prefer-const': 'error',
 
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
-
-
-
   },
-]
+];
